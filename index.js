a3c7f37f53866d6ae89c2b8a557aa79cd8faedda
olenazubashenko <olenazubashenko@gmail.com>
2019-09-11 11:18:22 +0300

    new changes

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 
-let i = 0;
+
 let images = ['./img/banner2.jpg','./img/banner3.jpg','./img/banner4.jpg'];
 
-document.getElementsByClassName('next')[0].onclick = ()=>{
-	if(i > 2) 
-        i = 0; 
-        let newImg  = images[i++];
-        let oldImg =  document.getElementById('top').style.backgroundImage ;
-        document.getElementById("top").style.backgroundImage = "url("+ newImg +")";
+document.getElementsByClassName('next')[0].onclick = () => {
+        setImage(1);
 };
-document.getElementsByClassName('prev')[0].onclick = ()=>{
-	if(i < 0) 
-        i = 2; 
-        let newImg  = images[i--];
-        let elem =  document.getElementById('top');//.style.cssText;
-        document.getElementById("top").style.backgroundImage = "url("+ newImg +")";
+$('.prev')[0].onclick = () => {
+        setImage(-1);
 };
-
+let i = 0;
+let setImage = (step) => {  
+        i = i + step;     
+        if (i < 0)
+                i = images.length - 1;
+        if (i > images.length - 1)
+                i = 0;
+        let newImg = images[i];
+        let elem = $('#top')[0];
+        elem.style.backgroundImage = "url(" + newImg + ")";
+}
 
 $('.count').each(function () {
         $(this).prop('Counter',0).animate({
